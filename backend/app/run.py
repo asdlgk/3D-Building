fcf2f0589fcfeb759628e344273b8b8359e90945
asdlgk <135594403+asdlgk@users.noreply.github.com>
2025-03-14 14:35:41 +0800

    Create run.py

---
+++ backend/app/run.py
@@ -0,0 +1,14 @@
+import os
+from app import create_app
+
+app = create_app('production')
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "run:app",
+        host="0.0.0.0",
+        port=5000,
+        workers=4,
+        log_level="info"
+    )
